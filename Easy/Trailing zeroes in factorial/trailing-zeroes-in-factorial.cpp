c35528715bb2d45dd52f1a48839383b4c1f94332
code_runner_07 <110618721+Prakash333singh@users.noreply.github.com>
2023-10-10 00:01:29 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Trailing zeroes in factorial/trailing-zeroes-in-factorial.cpp
@@ -0,0 +1,37 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function Template for C++
+class Solution
+{
+public:
+    int trailingZeroes(int N)
+    {
+        int num=N,ans=0;
+        while(num>=5)
+        {
+          ans+=num/5;
+          num=num/5;  
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int N;
+        cin >> N;
+        Solution ob;
+        int ans  = ob.trailingZeroes(N);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
