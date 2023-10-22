006abdcc452230f7db8e79beac5827dd8aee283d
code_runner_07 <110618721+Prakash333singh@users.noreply.github.com>
2023-10-22 14:52:59 +0530

    Updated Solution - GfG to GitHub

--- Easy/Intersection of two arrays/intersection-of-two-arrays.cpp
+++ Easy/Intersection of two arrays/intersection-of-two-arrays.cpp
@@ -12,25 +12,28 @@ class Solution {
     // Function to return the count of the number of elements in
     // the intersection of two arrays.
     int NumberofElementsInIntersection(int a[], int b[], int n, int m) {
-          
-        int ans = 0,count = 0;
-        int size;
-        if(m>n)
-        size = m;
-        else 
-        size = n;
+        // int count=0;
+       
+        // unordered_map<int ,int>mp;
         
-        unordered_map<int,int>mp;
-        for(int i = 0;i<n;i++){
-            mp[a[i]]++;
+        // for(int i=0;i<n;i++){
+        //     mp[a[i]]++;
+        // }
+        // for(int i=0;i<m;i++){
+        //     if(mp.find(b[i])!=mp.end())
+        //     count++;
+        //       mp.erase(b[i]);
+        // }
+        // return count;
+        int count=0;
+        unordered_set<int>s1(a,a+n);
+        unordered_set<int>s2(b,b+m);
+        
+        for(auto &x :s2){
+           if(s1.find(x)!=s1.end())
+           count++;
         }
-        for(int i = 0;i<m;i++){
-            if(mp.find(b[i])!=mp.end())
-            {
-                count++;
-                mp.erase(b[i]);
-            }
-        }
+        
         return count;
     }
 };
