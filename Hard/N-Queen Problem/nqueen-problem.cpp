d444e0abe83c797d65471e1e37510d866889ea17
code_runner_07 <110618721+Prakash333singh@users.noreply.github.com>
2023-12-09 12:30:10 +0530

    Updated Solution - GfG to GitHub

--- Hard/N-Queen Problem/nqueen-problem.cpp
+++ Hard/N-Queen Problem/nqueen-problem.cpp
@@ -7,75 +7,87 @@ using namespace std;
 // } Driver Code Ends
 // User function Template for C++
 
-
 class Solution{
 public:
-void addtosolution(vector<vector<int>> &ans,vector<vector<int>> &board,int n){
-vector<int> tem;
-for(int i =0;i<n;i++){
-for(int j =0;j<n;j++){
-   if(board[j][i])
-tem.push_back(j+1);
-}
- 
-}
-ans.push_back(tem);
-}
-bool issafe(int r, int c,vector<vector<int>> &board,int n){
-int x=r;
-int y =c;
-//row checking
-while(y>=0){
-if(board[x][y] == 1) return false;
-y--;
-}
-//uppar daigonal
-x=r;
-y =c;
-while(x>=0 and y>=0){
-if(board[x][y] == 1) return false;
-x--;
-y--;
-}
-//lower diagonal
-x=r;
-y =c;
-while(x<n and y>=0){
-if(board[x][y] == 1) return false;
-x++;
-y--;
-}
-return true;
-}
-void solve(int col,vector<vector<int>> &ans,vector<vector<int>> &board,int n){
-if(col == n){
-addtosolution(ans,board,n);
-return;
-}
-for(int r =0;r<n;r++){
-                if (issafe(r, col, board, n)) {
-                        board[r][col] = 1;
-                        solve(col + 1, ans, board, n);
-                        board[r][col] = 0;
-                }
+   bool isQueenSafe(int row, int col, vector<vector<int>> &board, int n) {
+    // for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+    //     if (board[i][j] == 1) {
+    //         return false;
+    //     }
+    // }
+
+    // for (int i = row - 1, j = col; i >= 0; i--) {
+    //     if (board[i][j] == 1) {
+    //         return false;
+    //     }
+    // }
+
+    // for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+    //     if (board[i][j] == 1) {
+    //         return false;
+    //     }
+    // }
+
+    // for (int i = row, j = col - 1; j >= 0; j--) {
+    //     if (board[i][j] == 1) {
+    //         return false;
+    //     }
+    // }
+
+    // return true;
+     int x = row;
+        int y = col;
+        while(y >= 0)
+        {
+            if(board[x][y] == 1)
+                return false;
+            y--;   
         }
+        x = row;
+        y = col;
+        while(x >= 0 && y >= 0)
+        {
+            if(board[x][y] == 1)
+                return false;
+            x--;
+            y--;
+        }
+        x = row;
+        y = col;
+        while(x < n && y >= 0)
+        {
+            if(board[x][y] == 1)
+                return false;
+            x++;
+            y--;
+        }
+        return true;
 }
-    vector<vector<int>> nQueen(int n) {
-       vector<vector<int>> ans;
-       vector<vector<int>> ans1(n,vector<int>(1,-1));
-       if(n==1){
-        vector<vector<int>> ans2(n,vector<int>(1,1)); 
-        ans2.resize(1,vector<int>(0));
-        return ans2;
-       }
-       
-       ans1.resize(0,vector<int>(0));
-       vector<vector<int>>board(n,vector<int>(n,0));
-       solve(0,ans,board,n);
-       if(n==3||n==2) return ans1;
-       return ans;
-       
+
+void printNQueens(int col, vector<vector<int>> &ans, vector<vector<int>> &board, vector<int> &temp, int n) {
+    if (col == n) {
+        ans.push_back(temp);
+        return;
     }
+
+    for (int row = 0; row < n; row++) {
+        if (board[row][col] == 0 && isQueenSafe(row, col, board, n)) {
+            temp.push_back(row + 1);
+            board[row][col] = 1;
+            printNQueens(col + 1, ans, board, temp, n);
+            board[row][col] = 0;
+            temp.pop_back();
+        }
+    }
+}
+
+vector<vector<int>> nQueen(int n) {
+    vector<vector<int>> board(n, vector<int>(n, 0));
+    vector<vector<int>> ans;
+    vector<int> temp;
+    printNQueens(0, ans, board, temp, n);
+    return ans;
+}
 };
 
 //{ Driver Code Starts.
