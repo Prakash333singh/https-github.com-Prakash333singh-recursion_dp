0b67b345e86e4e1b553b57017c5de09fd8013e65
code_runner_07 <110618721+Prakash333singh@users.noreply.github.com>
2024-01-23 01:46:31 +0530

    Updated Solution - GfG to GitHub

--- Medium/Minimum Platforms/minimum-platforms.cpp
+++ Medium/Minimum Platforms/minimum-platforms.cpp
@@ -12,26 +12,27 @@ class Solution{
     //railway station such that no train waits.
     int findPlatform(int arr[], int dep[], int n)
     {
-       int max_=0;
-      int count=0;
-      sort(arr,arr+n);
-      sort(dep,dep+n);
-      int i=0,j=0;
-      while(i<n)
-      {
-          if(arr[i]<=dep[j]) 
+    
+        sort(arr, arr + n);
+        sort(dep, dep + n);
+        
+         int platform= 1;
+         int i=1;
+         int j=0;
+         while(i<n &&j<n)
           {
-              count++;
-              i++;
-          }
-          else
-          {
-              count--;
-              j++;
-          }
-          max_=max(count,max_);
-      }
-      return max_;
+             if(arr[i]>dep[j])
+             {
+                 j++;
+             }
+             else
+             {
+                 platform++;
+             }
+             
+             i++;
+         }
+    	return platform;
     }
 };
 
