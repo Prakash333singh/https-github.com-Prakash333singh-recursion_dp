664ed3ae801452d03c5701685176f78206cbff8e
code_runner_07 <110618721+Prakash333singh@users.noreply.github.com>
2024-06-25 17:22:27 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -0,0 +1,69 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include<bits/stdc++.h> 
+using namespace std; 
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    bool isSubsetSum(vector<int>arr, int sum){
+    int n = arr.size();
+    bool t[n + 1][sum + 1];
+
+    // Initialize the dp table
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= sum; j++)
+        {
+            if (i == 0)
+                t[i][j] = false; // If there are no elements, we can't form any sum except 0
+            if (j == 0)
+                t[i][j] = true; // Sum 0 can always be formed with 0 elements
+        }
+    }
+
+    // Fill the dp table
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= sum; j++)
+        {
+            if (arr[i - 1] <= j)
+            {
+                t[i][j] = t[i - 1][j - arr[i - 1]] || t[i - 1][j];
+            }
+            else
+            {
+                t[i][j] = t[i - 1][j];
+            }
+        }
+    }
+
+    return t[n][sum];
+    }
+};
+
+//{ Driver Code Starts.
+int main() 
+{ 
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int N, sum;
+        cin >> N;
+        vector<int> arr(N);
+        for(int i = 0; i < N; i++){
+            cin >> arr[i];
+        }
+        cin >> sum;
+        
+        Solution ob;
+        cout << ob.isSubsetSum(arr, sum) << endl;
+    }
+    return 0; 
+} 
+
+// } Driver Code Ends
