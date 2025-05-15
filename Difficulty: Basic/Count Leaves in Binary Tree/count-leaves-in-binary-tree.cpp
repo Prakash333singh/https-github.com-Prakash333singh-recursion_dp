ac340a8a99b583aef36b98ab5d1698ad492a1c4e
code_runner_07 <110618721+Prakash333singh@users.noreply.github.com>
2025-05-15 10:21:22 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Count Leaves in Binary Tree/count-leaves-in-binary-tree.cpp
+++ Difficulty: Basic/Count Leaves in Binary Tree/count-leaves-in-binary-tree.cpp
@@ -2,106 +2,85 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct Node
-{
+// Tree Node
+struct Node {
     int data;
-    struct Node *left;
-    struct Node *right;
+    struct Node* left;
+    struct Node* right;
 };
-Node* newNode(int val)
-{
+
+// Function to create a new node
+Node* newNode(int val) {
     Node* temp = new Node;
     temp->data = val;
     temp->left = NULL;
     temp->right = NULL;
-    
     return temp;
 }
-Node* buildTree(string str)
-{   
+
+// Function to build the tree from a string input
+Node* buildTree(string str) {
     // Corner Case
-    if(str.length() == 0 || str[0] == 'N')
-            return NULL;
-    
-    // Creating vector of strings from input 
-    // string after spliting by space
+    if (str.length() == 0 || str[0] == 'N')
+        return NULL;
+
+    // Creating a vector of strings from the input string
     vector<string> ip;
-    
     istringstream iss(str);
-    for(string str; iss >> str; )
+    for (string str; iss >> str;)
         ip.push_back(str);
-        
+
     // Create the root of the tree
     Node* root = newNode(stoi(ip[0]));
-        
+
     // Push the root to the queue
     queue<Node*> queue;
     queue.push(root);
-        
+
     // Starting from the second element
     int i = 1;
-    while(!queue.empty() && i < ip.size()) {
-            
+    while (!queue.empty() && i < ip.size()) {
         // Get and remove the front of the queue
         Node* currNode = queue.front();
         queue.pop();
-            
+
         // Get the current node's value from the string
         string currVal = ip[i];
-            
+
         // If the left child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
             // Create the left child for the current node
             currNode->left = newNode(stoi(currVal));
-                
             // Push it to the queue
             queue.push(currNode->left);
         }
-            
+
         // For the right child
         i++;
-        if(i >= ip.size())
+        if (i >= ip.size())
             break;
         currVal = ip[i];
-            
+
         // If the right child is not null
-        if(currVal != "N") {
-                
+        if (currVal != "N") {
             // Create the right child for the current node
             currNode->right = newNode(stoi(currVal));
-                
             // Push it to the queue
             queue.push(currNode->right);
         }
         i++;
     }
-    
+
     return root;
 }
-int countLeaves(struct Node* root);
 
-int main()
-{
-    int t;
-	scanf("%d ",&t);
-    while(t--)
-    {
-        string s;
-		getline(cin,s);
-		Node* root = buildTree(s);
-		cout<< countLeaves(root)<<endl;
-    }
-    return 0;
-}
 
 // } Driver Code Ends
 
-
-//User function Template for C++
+// User function Template for C++
 
 /* A binary tree node has data, pointer to left child
-   and a pointer to right child  
+   and a pointer to right child
 struct Node
 {
     int data;
@@ -109,29 +88,46 @@ struct Node
     Node* right;
 }; */
 
-/* Should return count of leaves. For example, return
-    value should be 2 for following tree.
-         10
-      /      \ 
-   20       30 */
-   void f(Node* root,int &ans){
-       if(root==NULL)return;
+// Class Solution
+class Solution {
+  public:
+    // Function to count the number of leaf nodes in a binary tree.
+    int solve(Node* root){
+        // int count=0;
+        
+        //base case no tree is there 
+       if(root==NULL)return 0;
+       
        if(root->left==NULL && root->right==NULL)
        {
-           ans++;
-           return;
+           return 1;
        }
        
-       f(root->left,ans);
-       f(root->right,ans);
-   }
-   
-int countLeaves(Node* root)
-{
- 
-  int res = 0;
-    f(root,res);
-  return res;
-  
-  
+       return solve(root->left) + solve(root->right);
+    }
+    
+    int countLeaves(Node* root) {
+        
+        return solve(root);
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    scanf("%d ", &t);
+    Solution solution; // Create an instance of Solution
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        cout << solution.countLeaves(root)
+             << endl; // Call countLeaves on the Solution instance
+        cout << "~" << endl;
+    }
+    return 0;
 }
+
+// } Driver Code Ends
